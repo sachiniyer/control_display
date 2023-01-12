7e463480cc45e4cc9dae2b01afe3bd7688e5aa65
Sachin Iyer <sachinjiyer@gmail.com>
2023-01-12 14:15:13 -0500

    modify script to call site

--- control_display_host/script.py
+++ control_display_host/script.py
@@ -1,9 +1,26 @@
 import serial
 import time
+import requests
 
 ser = serial.Serial("/dev/ttyUSB0", 9600, timeout = 1)
 time.sleep(2)
 
-# Send character 'S' to start the program
-command = str.encode('ADASDO')
-ser.write(command)
+val = ""
+
+
+def sendData(data):
+    """
+    This command sends data over the serial port
+    """
+    command = str.encode(data)
+    ser.write(command)
+
+
+while True:
+    x = requests.get('http://localhost:3000/api/word')
+    x = x.text
+    if x != val:
+        val = x
+        sendData(val)
+        print(val)
+    time.sleep(5)
